96a41d02cf6b6b046140e97f263ee7b396ff700e
keung0606 <keung123654@gmail.com>
2023-06-17 16:04:37 +0800

    Add the search function

--- src/Cats.jsx
+++ src/Cats.jsx
@@ -4,10 +4,15 @@ import { Link } from 'react-router-dom';
 
 function Cats() {
   const [cats, setCats] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredCats, setFilteredCats] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:3002')
-      .then(result => setCats(result.data))
+      .then(result => {
+        setCats(result.data);
+        setFilteredCats(result.data);
+      })
       .catch(err => console.log(err));
   }, []);
 
@@ -20,10 +25,41 @@ function Cats() {
       .catch(err => console.log(err));
   };
 
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    filterCats(event.target.value);
+  };
+
+  const filterCats = (searchTerm) => {
+    const filteredCats = cats.filter(cat => {
+      const catName = cat.name.toLowerCase();
+      const catBreed = cat.breed.toLowerCase();
+      const catAge = cat.age.toString();
+      const catGender = cat.gender.toLowerCase();
+      const searchTermLower = searchTerm.toLowerCase();
+  
+      return (
+        catName.includes(searchTermLower) ||
+        catBreed.includes(searchTermLower) ||
+        catAge.includes(searchTermLower) ||
+        catGender.includes(searchTermLower)
+      );
+    });
+  
+    setFilteredCats(filteredCats);
+  };
+  
+
   return (
     <div>
       <div>
         <Link to="/create" className="btn_add">Add Cat</Link>
+        <input
+          type="text"
+          placeholder="Search cats by name"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
         <table className="table">
           <thead>
             <tr>
@@ -35,7 +71,7 @@ function Cats() {
             </tr>
           </thead>
           <tbody>
-            {cats.map(cat => (
+            {filteredCats.map(cat => (
               <tr key={cat._id}>
                 <td>{cat.name}</td>
                 <td>{cat.breed}</td>
@@ -55,3 +91,4 @@ function Cats() {
 }
 
 export default Cats;
+
